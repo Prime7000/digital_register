08193d37c9d548824c3f52ff67637e8594e644d6
prime7000 <agbadutaofejiro123@gmail.com>
2025-03-28 16:58:53 +0100

    prime firsrt commit

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'my_name_is_prime'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///digital_register.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+#user model:
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    role = db.Column(db.String(20), nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+
+    def __str__(self):
+        return f'<User {self.username}>'
+    
+with app.app_context():
+    db.create_all()
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))  # Adjust based on your ORM
+
+# ________________________________________________________________________________________________________
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# ________________________________________________________________________________________________________
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+
+
