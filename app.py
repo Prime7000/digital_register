1e2639b5ecf54365a6faa90ac7b41c9d41dfa274
prime7000 <agbadutaofejiro123@gmail.com>
2025-03-28 23:41:39 +0100

    finish signup and login page

--- app.py
+++ app.py
@@ -38,6 +38,13 @@ def load_user(user_id):
 def index():
     return render_template('index.html')
 
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 # ________________________________________________________________________________________________________
 if __name__ == '__main__':
     app.run(debug=True)
